1dc5a0650959ff3853cf665f695f3f23103a4ab1
Aniket-Bhat <69003723+Aniket-Bhat@users.noreply.github.com>
2025-03-26 02:13:40 +0530

    Init

---
+++ README.md
@@ -0,0 +1,13 @@
+# This is a template for creating a vite application
+
+## Content covered
+- Vite (v6.2)
+- React (v19)
+- tsconfig (v5.7)
+- Tailwind (v4)
+- Github Action
+- start.sh (needed for [run.sh](https://gist.github.com/Aniket-Bhat/4d55615faf5fd10f8d3e0e7b0c24eaed))
+- Shad/cn
+- React router
+- Generic Landing page (soon)
+
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+import { imagetools } from 'vite-imagetools'
+
+// https://vite.dev/config/
+export default defineConfig({
+	plugins: [react(), tailwindcss(), imagetools()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+})
